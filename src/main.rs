988215a16294490a0d5c041194eb886559647569
AJTJ <janke.aaron@gmail.com>
2021-07-25 22:05:19 -0500

    update Sun 25 Jul 2021 22:05:19 CDT

--- src/main.rs
+++ src/main.rs
@@ -2,84 +2,90 @@ use std::io;
 
 fn main() {
     // THREE "STACKS"
-    let mut vec_1: Vec<i32> = vec![3, 2, 1];
-    let mut vec_2: Vec<i32> = vec![];
-    let mut vec_3: Vec<i32> = vec![];
+    let mut vec_1: Vec<usize> = vec![3, 2, 1];
+    let mut vec_2: Vec<usize> = vec![];
+    let mut vec_3: Vec<usize> = vec![];
 
-    let vec_refs: [&mut Vec<i32>; 3] = [&mut vec_1, &mut vec_2, &mut vec_3];
+    let vec_refs: [&mut Vec<usize>; 3] = [&mut vec_1, &mut vec_2, &mut vec_3];
 
     // INSTRUCTIONS
-    println!("");
+    println!();
     println!("In the spirit of the tower of Hanoi, move all numbers (discs) from the 1st set (rod) to the 3rd. You can only move one number at a time and you are only able to move the top (right-most) number from any given set. You cannot put a larger number on a smaller number");
 
-    fn set_target(target: &mut i32, vec_refs: &[&mut Vec<i32>; 3], origin: i32) -> bool {
+    fn set_target(target: &mut usize, vec_refs: &[&mut Vec<usize>; 3], origin: usize) -> bool {
         let mut buf = String::new();
         io::stdin().read_line(&mut buf).unwrap();
-        match buf.trim_end().parse::<i32>() {
+        match buf.trim_end().parse::<usize>() {
             // input exists
             Ok(input) => {
                 // check if input is out of bounds
-                if (input as usize) > vec_refs.len() || input <= 0 {
-                    println!("");
+                if input > vec_refs.len() || input <= 0 {
+                    println!();
                     println!("***out of bounds***");
-                    println!("");
+                    println!();
                     return false;
-                } else {
+                } else if origin > 0 {
                     // origin will be greater than 0 while setting the destination
-                    if origin > 0 {
-                        // check if distination is also the origin
-                        if input == origin {
-                            println!("");
-                            println!("***distination is the same as origin***");
-                            println!("");
-                            return false;
-                        }
-                        let dest_vec = &vec_refs[(input as usize) - 1];
-                        match dest_vec.last() {
-                            Some(dest_disc) => {
-                                // check if the moved number is smaller than than any numbers it is landing on
-                                if let Some(ori_disc) = &vec_refs[(origin as usize) - 1].last() {
-                                    if dest_disc < ori_disc {
-                                        println!("");
-                                        println!(
-                                            "***cannot place larger disc ({}) on smaller disc ({})***",
-                                            input, dest_disc
-                                        );
-                                        println!("");
-                                        return false;
-                                    }
-                                }
-                            }
-                            None => (),
-                        }
-                    }
-
+                    check_destination(target, vec_refs, origin, input)
+                } else {
                     *target = input;
                     true
                 }
             }
             Err(_) => {
-                println!("");
+                println!();
                 println!("***incorrect input***");
-                println!("");
+                println!();
                 false
             }
         }
     }
 
-    fn display_vectors(vec_refs: &[&mut Vec<i32>; 3]) {
-        println!("");
+    fn check_destination(
+        target: &mut usize,
+        vec_refs: &[&mut Vec<usize>; 3],
+        origin: usize,
+        input: usize,
+    ) -> bool {
+        // check if distination is also the origin
+        if input == origin {
+            println!();
+            println!("***destination is the same as origin***");
+            println!();
+            return false;
+        }
+        let dest_vec = &vec_refs[input - 1];
+        if let Some(dest_disc) = dest_vec.last() {
+            // check if the moved number is smaller than than any numbers it is landing on
+            if let Some(ori_disc) = &vec_refs[origin - 1].last() {
+                if dest_disc < ori_disc {
+                    println!();
+                    println!(
+                        "***cannot place larger disc ({}) on smaller disc ({})***",
+                        input, dest_disc
+                    );
+                    println!();
+                    return false;
+                }
+            }
+        }
+        *target = input;
+        true
+    }
+
+    fn display_vectors(vec_refs: &[&mut Vec<usize>; 3]) {
+        println!();
         println!("1: {:?}", vec_refs[0]);
         println!("2: {:?}", vec_refs[1]);
         println!("3: {:?}", vec_refs[2]);
-        println!("");
+        println!();
     }
 
     // GAME LOOP
     loop {
         // INPUT LOGIC
-        let mut origin: i32 = 0;
-        let mut destination: i32 = 0;
+        let mut origin: usize = 0;
+        let mut destination: usize = 0;
 
         display_vectors(&vec_refs);
 
@@ -94,15 +100,15 @@ fn main() {
         }
 
         // POP AND PLACE
-        let popped = &vec_refs[(origin - 1) as usize].pop().unwrap();
-        vec_refs[(destination - 1) as usize].push(popped.clone());
+        let popped = &vec_refs[origin - 1].pop().unwrap();
+        vec_refs[destination - 1].push(popped.clone());
 
-        let final_vec: Vec<i32> = vec![3, 2, 1];
+        let final_vec: Vec<usize> = vec![3, 2, 1];
         if *vec_refs[2] == final_vec {
             display_vectors(&vec_refs);
-            println!("");
+            println!();
             println!("You did it!");
-            println!("");
+            println!();
             break;
         }
     }
